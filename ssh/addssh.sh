81e52d7972832e226250258b4fa1c1587c21ab73
Muhammad Amin <96859296+geovpn@users.noreply.github.com>
2022-02-06 07:44:27 +0800

    Update addssh.sh

--- ssh/addssh.sh
+++ ssh/addssh.sh
@@ -4,25 +4,83 @@
 # Color
 RED='\033[0;31m'
 NC='\033[0m'
-GREEN='\033[0;32m'
-ORANGE='\033[0;33m'
+#GREEN='\033[0;32m'
+#ORANGE='\033[0;33m'
 BLUE='\033[0;34m'
 PURPLE='\033[0;35m'
-CYAN='\033[0;36m'
+#CYAN='\033[0;36m'
 LIGHT='\033[0;37m'
+off='\x1b[m'
 # ==========================================
 # Getting
-MYIP=$(wget -qO- ipinfo.io/ip);
+MYIP=$(curl -sS ipinfo.io/ip)
 echo "Checking VPS"
-IZIN=$( curl https://raw.githubusercontent.com/geovpn/perizinan/main/ip | grep $MYIP )
-if [ $MYIP = $IZIN ]; then
-echo -e "${NC}${GREEN}Permission Accepted...${NC}"
+# Getting
+dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
+biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
+#########################
+
+BURIQ () {
+    curl -sS https://raw.githubusercontent.com/geovpn/perizinan/main/main/allow > /root/tmp
+    data=( `cat /root/tmp | grep -E "^### " | awk '{print $2}'` )
+    for user in "${data[@]}"
+    do
+    exp=( `grep -E "^### $user" "/root/tmp" | awk '{print $3}'` )
+    d1=(`date -d "$exp" +%s`)
+    d2=(`date -d "$biji" +%s`)
+    exp2=$(( (d1 - d2) / 86400 ))
+    if [[ "$exp2" -le "0" ]]; then
+    echo $user > /etc/.$user.ini
+    else
+    rm -f /etc/.$user.ini > /dev/null 2>&1
+    fi
+    done
+    rm -f /root/tmp
+}
+
+MYIP=$(curl -sS ipinfo.io/ip)
+Name=$(curl -sS https://raw.githubusercontent.com/geovpn/perizinan/main/main/allow | grep $MYIP | awk '{print $2}')
+echo $Name > /usr/local/etc/.$Name.ini
+CekOne=$(cat /usr/local/etc/.$Name.ini)
+
+Bloman () {
+if [ -f "/etc/.$Name.ini" ]; then
+CekTwo=$(cat /etc/.$Name.ini)
+    if [ "$CekOne" = "$CekTwo" ]; then
+        res="Expired"
+    fi
 else
-echo -e "${NC}${RED}Permission Denied!${NC}";
-echo -e "${NC}${LIGHT}Please Contact Admin!!"
-echo -e "${NC}${LIGHT}Telegram : https://t.me/geovpn"
+res="Permission Accepted..."
+fi
+}
+
+PERMISSION () {
+    MYIP=$(curl -sS ipinfo.io/ip)
+    IZIN=$(curl -sS https://raw.githubusercontent.com/geovpn/perizinan/main/main/allow | awk '{print $4}' | grep $MYIP)
+    if [ "$MYIP" = "$IZIN" ]; then
+    Bloman
+    else
+    res="Permission Denied!"
+    fi
+    BURIQ
+}
+red='\e[1;31m'
+green='\e[1;32m'
+NC='\e[0m'
+green() { echo -e "\\033[32;1m${*}\\033[0m"; }
+red() { echo -e "\\033[31;1m${*}\\033[0m"; }
+PERMISSION
+
+if [ -f /home/needupdate ]; then
+red "Your script need to update first !"
+exit 0
+elif [ "$res" = "Permission Accepted..." ]; then
+echo -ne
+else
+red "Permission Denied!"
 exit 0
 fi
+clear
 source /var/lib/geovpnstore/ipvps.conf
 if [[ "$IP2" = "" ]]; then
 domain=$(cat /etc/xray/domain)
@@ -34,55 +92,43 @@ read -p "Username : " Login
 read -p "Password : " Pass
 read -p "Expired (Days): " masaaktif
 
-IP=$(wget -qO- ipinfo.io/ip);
+MYIP=$(curl -sS ipinfo.io/ip)
 ssl="$(cat ~/log-install.txt | grep -w "Stunnel5" | cut -d: -f2)"
 sqd="$(cat ~/log-install.txt | grep -w "Squid" | cut -d: -f2)"
 ovpn="$(netstat -nlpt | grep -i openvpn | grep -i 0.0.0.0 | awk '{print $4}' | cut -d: -f2)"
 ovpn2="$(netstat -nlpu | grep -i openvpn | grep -i 0.0.0.0 | awk '{print $4}' | cut -d: -f2)"
 clear
-systemctl restart ws-tls
-systemctl restart ws-nontls
-systemctl restart ssh-ohp
-systemctl restart dropbear-ohp
-systemctl restart openvpn-ohp
 useradd -e `date -d "$masaaktif days" +"%Y-%m-%d"` -s /bin/false -M $Login
 expi="$(chage -l $Login | grep "Account expires" | awk -F": " '{print $2}')"
 echo -e "$Pass\n$Pass\n"|passwd $Login &> /dev/null
 hariini=`date -d "0 days" +"%Y-%m-%d"`
 expi=`date -d "$masaaktif days" +"%Y-%m-%d"`
 echo -e ""
-echo -e "Informasi SSH & OpenVPN"
-echo -e "=============================="
-echo -e "IP/Host       : $IP"
-echo -e "Domain        : ${domain}"
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "Thank You For Using Our Services SSH ${off}"
+echo -e "OpenVPN & Websocket Account Info${off}"
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e "Username      : $Login"
 echo -e "Password      : $Pass"
+echo -e "Created       : $hariini"
+echo -e "Expired       : $expi ${off}"
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "IP/Host       : ${domain} / $MYIP"
 echo -e "OpenSSH       : 443, 22"
 echo -e "Dropbear      : 443, 109, 143"
 echo -e "SSL/TLS       :$ssl"
 echo -e "Port Squid    :$sqd"
-echo -e "OHP SSH       : 8181"
-echo -e "OHP Dropbear  : 8282"
-echo -e "OHP OpenVPN   : 8383"
-echo -e "Ssh Ws SSL    : 443"
-echo -e "Ssh Ws No SSL : 8880"
-echo -e "Ovpn Ws       : 2086"
-echo -e "Port TCP      : $ovpn"
-echo -e "Port UDP      : $ovpn2"
-echo -e "Port SSL      : 990"
-echo -e "OVPN TCP      : http://$IP:89/tcp.ovpn"
-echo -e "OVPN UDP      : http://$IP:89/udp.ovpn"
-echo -e "OVPN SSL      : http://$IP:89/ssl.ovpn"
-echo -e "BadVpn        : 7100-7200-7300"
-echo -e "Created       : $hariini"
-echo -e "Expired       : $expi"
-echo -e "=============================="
-echo -e "Payload Websocket TLS"
-echo -e "=============================="
-echo -e "GET wss://bug.com [protocol][crlf]Host: ${domain}[crlf]Upgrade: websocket[crlf][crlf]"
-echo -e "=============================="
-echo -e "Payload Websocket No TLS"
-echo -e "=============================="
-echo -e "GET / HTTP/1.1[crlf]Host: ${domain}[crlf]Upgrade: websocket[crlf][crlf]"
-echo -e "=============================="
-echo -e "Script By geovpn"
+echo -e "OHP           : SSH 8181, Dropbear 8282, OpenVPN 8383${off}"
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "SSH WS        : 2052, 2095"
+echo -e "SSH WS SSL    : 443, 2096"
+echo -e "OpenVPN WS    : 2086${off}"
+echo -e "\033[0;34m===Payload CDN Websocket===\033[0m"
+echo -e "GET / HTTP/1.1[crlf]Host: ${domain}[crlf]Connection: Keep-Alive[crlf]User-Agent: [ua][crlf]Upgrade: websocket[crlf][crlf]${off}"
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "Link Ovpn     : http://$MYIP:89/"
+echo -e "badvpn        : 7100-7200-7300${off}"
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e ""
+echo -e "${NC}Script By geovpn${off}"
+echo -e ""
