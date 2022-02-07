68946124afa512e928b127613887532a2b30a75c
Muhammad Amin <96859296+geovpn@users.noreply.github.com>
2022-02-06 18:48:30 -0800

    Update trialv2ray.sh

--- xray/trialv2ray.sh
+++ xray/trialv2ray.sh
@@ -1,15 +1,80 @@
 #!/bin/bash
-grey='\x1b[90m'
-red='\x1b[91m'
-green='\x1b[92m'
-yellow='\x1b[93m'
-blue='\x1b[94m'
-purple='\x1b[95m'
-cyan='\x1b[96m'
-white='\x1b[37m'
-bold='\033[1m'
-off='\x1b[m'
-flag='\x1b[47;41m'
+# My Telegram : https://t.me/sampiiiiu
+# ==========================================
+# Color
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHT='\033[0;37m'
+# ==========================================
+dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
+biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
+#########################
+
+BURIQ () {
+    curl -sS https://raw.githubusercontent.com/geovpn/perizinan/main/main/allow > /root/tmp
+    data=( `cat /root/tmp | grep -E "^### " | awk '{print $2}'` )
+    for user in "${data[@]}"
+    do
+    exp=( `grep -E "^### $user" "/root/tmp" | awk '{print $3}'` )
+    d1=(`date -d "$exp" +%s`)
+    d2=(`date -d "$biji" +%s`)
+    exp2=$(( (d1 - d2) / 86400 ))
+    if [[ "$exp2" -le "0" ]]; then
+    echo $user > /etc/.$user.ini
+    else
+    rm -f /etc/.$user.ini > /dev/null 2>&1
+    fi
+    done
+    rm -f /root/tmp
+}
+
+MYIP=$(curl -sS ipinfo.io/ip)
+Name=$(curl -sS https://raw.githubusercontent.com/geovpn/perizinan/main/main/allow | grep $MYIP | awk '{print $2}')
+echo $Name > /usr/local/etc/.$Name.ini
+CekOne=$(cat /usr/local/etc/.$Name.ini)
+
+Bloman () {
+if [ -f "/etc/.$Name.ini" ]; then
+CekTwo=$(cat /etc/.$Name.ini)
+    if [ "$CekOne" = "$CekTwo" ]; then
+        res="Expired"
+    fi
+else
+res="Permission Accepted..."
+fi
+}
+
+PERMISSION () {
+    MYIP=$(curl -sS ipinfo.io/ip)
+    IZIN=$(curl -sS https://raw.githubusercontent.com/geovpn/perizinan/main/main/allow | awk '{print $4}' | grep $MYIP)
+    if [ "$MYIP" = "$IZIN" ]; then
+    Bloman
+    else
+    res="Permission Denied!"
+    fi
+    BURIQ
+}
+red='\e[1;31m'
+green='\e[1;32m'
+NC='\e[0m'
+green() { echo -e "\\033[32;1m${*}\\033[0m"; }
+red() { echo -e "\\033[31;1m${*}\\033[0m"; }
+PERMISSION
+
+if [ -f /home/needupdate ]; then
+red "Your script need to update first !"
+exit 0
+elif [ "$res" = "Permission Accepted..." ]; then
+echo -ne
+else
+red "Permission Denied!"
+exit 0
+fi
 clear
 source /var/lib/geovpnstore/ipvps.conf
 if [[ "$IP" = "" ]]; then
@@ -70,9 +135,9 @@ systemctl restart xray@v2ray-tls
 systemctl restart xray@v2ray-nontls
 service cron restart
 clear
-echo -e "${cyan}==================================${off}"
-echo -e "${purple} ~> TRIAL XRAY / VMESS${off}"
-echo -e "${cyan}==================================${off}"
+echo -e " ==================================${off}"
+echo -e " TRIAL XRAY / VMESS${off}"
+echo -e " ==================================${off}"
 echo -e " Remarks        : ${user}"
 echo -e " Bug            : ${bug}"
 echo -e " Domain         : ${domain}"
@@ -83,13 +148,13 @@ echo -e " AlterID        : 0"
 echo -e " Security       : auto"
 echo -e " Network        : ws"
 echo -e " Path           : geo${off}"
-echo -e "${cyan}==================================${off}"
-echo -e "${purple}~> VMESS TLS : $off${xrayv2ray1}"
-echo -e "${cyan}==================================${off}"
-echo -e "${purple}~> VMESS NON-TLS : $off${xrayv2ray2}"
-echo -e "${cyan}==================================${off}"
-echo -e " ${green}Aktif Selama   : $masaaktif Hari"
-echo -e "${cyan}==================================${off}"
+echo -e " ==================================${off}"
+echo -e " VMESS TLS : $off${xrayv2ray1}"
+echo -e " ==================================${off}"
+echo -e " VMESS NON-TLS : $off${xrayv2ray2}"
+echo -e " ==================================${off}"
+echo -e " Aktif Selama   : $masaaktif Hari"
+echo -e " ==================================${off}"
 echo -e ""
 echo -e "Script By GeoVPN"
 echo -e ""
