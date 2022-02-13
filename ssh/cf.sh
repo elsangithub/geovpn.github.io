fedaa29248e7d3e072e7a097406d0cd7e3880b2b
Muhammad Amin <96859296+geovpn@users.noreply.github.com>
2022-02-13 10:30:54 +0800

    Update cf.sh

--- ssh/cf.sh
+++ ssh/cf.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
-# My Telegram : https://t.me/sampiiiiu
-# ==========================================
-# Color
-RED='\033[0;31m'
-NC='\033[0m'
-GREEN='\033[0;32m'
-ORANGE='\033[0;33m'
-BLUE='\033[0;34m'
-PURPLE='\033[0;35m'
-CYAN='\033[0;36m'
-LIGHT='\033[0;37m'
-# ==========================================
+red='\e[1;31m'
+green='\e[0;32m'
+NC='\e[0m'
+MYIP=$(wget -qO- ifconfig.me/ip);
 clear
 apt install jq curl -y
 DOMAIN=vpsku.space
