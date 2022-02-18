93b8972afd5ec47c888337bb660e499a666875c9
Muhammad Amin <96859296+geovpn@users.noreply.github.com>
2022-02-18 15:27:49 +0800

    Update trialssh.sh

--- trial/trialssh.sh
+++ trial/trialssh.sh
@@ -14,7 +14,6 @@ off='\x1b[m'
 # ==========================================
 # Getting
 MYIP=$(curl -sS ipinfo.io/ip)
-echo "Checking VPS"
 # Getting
 dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
 biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
