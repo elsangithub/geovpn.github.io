3817613e932c2d3f6b4f073477a92ef4e86d1d55
Muhammad Amin <96859296+geovpn@users.noreply.github.com>
2022-02-18 13:31:03 +0800

    Update menu.sh

--- menu/menu.sh
+++ menu/menu.sh
@@ -1,5 +1,17 @@
+#!/bin/bash
+yl='\e[32;1m'
+bl='\e[36;1m'
+gl='\e[32;1m'
+rd='\e[31;1m'
+mg='\e[0;95m'
+blu='\e[34m'
+op='\e[35m'
+or='\033[1;33m'
+bd='\e[1m'
+color1='\e[031;1m'
+color2='\e[34;1m'
+color3='\e[0m'
 # Getting
-clear
 dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
 biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
 #########################
@@ -85,104 +97,79 @@ echo -e ""
 #toilet --gay -f slant -t " Geo Project"
 #cat /usr/bin/bannerku | lolcat
 ISP=$(curl -s ipinfo.io/org | cut -d " " -f 2-10 )
-IPVPS=$(curl -s ipinfo.io/ip )
-DOMAIN=$(cat /etc/xray/domain)
-ISP=$(curl -s ipinfo.io/org | cut -d " " -f 2-10 )
-CITY=$(curl -s ipinfo.io/city )
-WKT=$(curl -s ipinfo.io/timezone )
-IPVPS=$(curl -s ipinfo.io/ip )
-jam=$(date +"%T")
-hari=$(date +"%A")
-tnggl=$(date +"%d-%B-%Y")
-	cname=$( awk -F: '/model name/ {name=$2} END {print name}' /proc/cpuinfo )
-	cores=$( awk -F: '/model name/ {core++} END {print core}' /proc/cpuinfo )
-	freq=$( awk -F: ' /cpu MHz/ {freq=$2} END {print freq}' /proc/cpuinfo )
-	tram=$( free -m | awk 'NR==2 {print $2}' )
-	swap=$( free -m | awk 'NR==4 {print $2}' )
-	up=$(uptime|awk '{ $1=$2=$(NF-6)=$(NF-5)=$(NF-4)=$(NF-3)=$(NF-2)=$(NF-1)=$NF=""; print }')
- echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
- echo -e "\E[44;1;39m                     ⇱ INFORMASI VPS ⇲                        \E[0m"
+#IPVPS=$(curl -s ipinfo.io/ip )
+#DOMAIN=$(cat /etc/xray/domain)
+#ISP=$(curl -s ipinfo.io/org | cut -d " " -f 2-10 )
+#CITY=$(curl -s ipinfo.io/city )
+#WKT=$(curl -s ipinfo.io/timezone )
+#IPVPS=$(curl -s ipinfo.io/ip )
+#jam=$(date +"%T")
+#hari=$(date +"%A")
+#tnggl=$(date +"%d-%B-%Y")
+	#cname=$( awk -F: '/model name/ {name=$2} END {print name}' /proc/cpuinfo )
+	#cores=$( awk -F: '/model name/ {core++} END {print core}' /proc/cpuinfo )
+	#freq=$( awk -F: ' /cpu MHz/ {freq=$2} END {print freq}' /proc/cpuinfo )
+	#tram=$( free -m | awk 'NR==2 {print $2}' )
+	#swap=$( free -m | awk 'NR==4 {print $2}' )
+	#up=$(uptime|awk '{ $1=$2=$(NF-6)=$(NF-5)=$(NF-4)=$(NF-3)=$(NF-2)=$(NF-1)=$NF=""; print }')
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "\E[44;1;39m                     ⇱ INFORMASI VPS ⇲                        \E[0m"
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e " ${color1} •${color3}$bd ISP Name          ${color1} :${color3}$bd $ISP"
- echo -e " ${color1} •${color3}$bd City              ${color1} :${color3}$bd $CITY"
- echo -e " ${color1} •${color3}$bd CPU Model         ${color1} :${color3}$bd$cname"
- echo -e " ${color1} •${color3}$bd Number Of Cores   ${color1} :${color3}$bd $cores"
- echo -e " ${color1} •${color3}$bd CPU Frequency     ${color1} :${color3}$bd$freq MHz"
- echo -e " ${color1} •${color3}$bd Total RAM         ${color1} :${color3}$bd $tram MB"
- echo -e " ${color1} •${color3}$bd Waktu             ${color1} :${color3}$bd $jam"
- echo -e " ${color1} •${color3}$bd Hari              ${color1} :${color3}$bd $hari"
- echo -e " ${color1} •${color3}$bd Tanggal           ${color1} :${color3}$bd $tnggl"
- echo -e " ${color1} •${color3}$bd IP VPS            ${color1} :${color3}$bd $IPVPS"
- echo -e " ${color1} •${color3}$bd Domain            ${color1} :${color3}$bd $DOMAIN"
- echo -e " ${color1} •${color3}$bd Client Name       ${color1} :${color3}$bd $Name"
- echo -e " ${color1} •${color3}$bd Expiry script     ${color1} :${color3}$bd $Exp"
- echo -e " ${color1} •${color3}$bd Version           ${color1} :${color3}$bd Latest Version"
- echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
- echo -e "\E[44;1;39m                     ⇱ MENU  OPTIONS ⇲                        \E[0m"
+echo -e " ${color1} •${color3}$bd Client Name       ${color1} :${color3}$bd $Name"
+echo -e " ${color1} •${color3}$bd Expiry script     ${color1} :${color3}$bd $Exp"
+echo -e " ${color1} •${color3}$bd Version           ${color1} :${color3}$bd Latest Version"
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "\E[44;1;39m                     ⇱ MENU  OPTIONS ⇲                        \E[0m"
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e "
-[\033[0;32m01\033[0m] • SSH & OpenVPN Section
-[\033[0;32m02\033[0m] • SSTP - L2TP - PPTP - Wireguard Section
-[\033[0;32m03\033[0m] • Shadowsocks & ShadowsocksR Section
-[\033[0;32m04\033[0m] • V2ray Vmess & Vless Section
-[\033[0;32m05\033[0m] • Trojan & TrojanGO Section
-[\033[0;32m06\033[0m] • Backup Section
-[\033[0;32m07\033[0m] • Trial Account Section
-[\033[0;32m08\033[0m] • System Tools Section
+[\033[0;32m01\033[0m] •$bd SSH & OpenVPN Section (${color2}menu-ssh${color3})
+[\033[0;32m02\033[0m] •$bd SSTP - L2TP - PPTP - Wireguard Section (${color2}menu-vpn${color3})
+[\033[0;32m03\033[0m] •$bd Shadowsocks & ShadowsocksR Section (${color2}menu-shadowsocks${color3})
+[\033[0;32m04\033[0m] •$bd V2ray Vmess & Vless Section (${color2}menu-v2ray${color3})
+[\033[0;32m05\033[0m] •$bd Trojan & TrojanGO Section (${color2}menu-trojan${color3})
+[\033[0;32m06\033[0m] •$bd Backup Section (${color2}menu-backup${color3})
+[\033[0;32m07\033[0m] •$bd Trial Account Section (${color2}menu-trial${color3})
+[\033[0;32m08\033[0m] •$bd System Tools Section (${color2}menu-tools${color3})
 
-[00] • Back to exit Menu \033[1;32m<\033[1;33m<\033[1;31m<\033[1;31m"
+[${color1}00${color3}] • Back to exit Menu \033[1;32m<\033[1;33m<\033[1;31m<\033[1;31m"
 echo ""
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo ""
-echo -ne "Select menu : "; read x
-
-case "$x" in 
-   1 | 01)
-   clear
-   menu-ssh
-   break
-   ;;
-   2 | 02)
-   clear
-   menu-vpn
-   break
-   ;;
-   3 | 03)
-   clear
-   menu-shadowsocks
-   break
-   ;;
-   4 | 04)
-   clear
-   menu-v2ray
-   break
-   ;;
-   5 | 05)
-   clear
-   menu-trojan
-   break
-   ;;
-   6 | 06)
-   clear
-   menu-backup
-   break
-   ;;
-   7 | 07)
-   clear
-   menu-trial
-   break
-   ;;
-   8 | 08)
-   clear
-   menu-tools
-   break
-   ;;
-   0 | 00)
-   clear
-   exit
-   ;;
-   *)
-   menu
+ read -p " Select menu :  " menu
+echo -e   ""
+case $menu in
+1)
+menu-ssh
+;;
+2)
+menu-vpn
+;;
+3)
+menu-shadowsocks
+;;
+4)
+menu-v2ray
+;;
+5)
+menu-trojan
+;;
+6)
+menu-backup
+;;
+7)
+menu-trial
+;;
+8)
+menu-tools
+;;
+9)
+installbot
+;;
+0 | 00)
+exit
+;;
+*)
+menu
+;;
 esac
-
-#fim
