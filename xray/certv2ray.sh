877bb44e7e3c66b6da6660697def717da26d72b2
Muhammad Amin <96859296+geovpn@users.noreply.github.com>
2022-02-24 06:41:03 +0800

    Update certv2ray.sh

--- xray/certv2ray.sh
+++ xray/certv2ray.sh
@@ -25,4 +25,7 @@ cd .acme.sh
 echo "starting...., Port 80 Akan di Hentikan Saat Proses install Cert"
 bash acme.sh --register-account -m patajunit@gmail.com
 bash acme.sh --issue --standalone -d $domain --force
-bash acme.sh --installcert -d $domain --fullchainpath /etc/xray/xray.crt --keypath /etc/x>restart
+bash acme.sh --installcert -d $domain
+--fullchainpath /etc/xray/xray.crt
+--keypath /etc/xray/xray.key
+restart
