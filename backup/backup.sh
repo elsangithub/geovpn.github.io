b7a524307ee584d729b438ed8febabce8842f12a
Muhammad Amin <96859296+geovpn@users.noreply.github.com>
2022-02-25 17:10:55 +0800

    Add files via upload

--- backup/backup.sh
+++ backup/backup.sh
@@ -132,7 +132,7 @@ Save_And_Exit () {
     git commit -m m &> /dev/null
     git branch -M main &> /dev/null
     git remote add origin https://github.com/geovpn/user-backup-db
-    git push -f https://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@github.com/geovpn/user-backup-db.git &> /dev/null
+    git push -f https://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@github.com/geovpn/user-backup-db.git &> /dev/null
 }
 
 if [ ! -d "/root/user-backup/" ]; then
