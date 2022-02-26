a269489d70cf06338a79bc84dff3a7d83fd02228
Muhammad Amin <96859296+geovpn@users.noreply.github.com>
2022-02-26 15:38:03 +0800

    Update backup.sh

--- backup/backup.sh
+++ backup/backup.sh
@@ -90,7 +90,7 @@ cp -r /etc/wireguard /root/backup/wireguard &> /dev/null
 cp /etc/ppp/chap-secrets /root/backup/chap-secrets &> /dev/null
 cp /etc/ipsec.d/passwd /root/backup/passwd1 &> /dev/null
 cp /etc/shadowsocks-libev/akun.conf /root/backup/ss.conf &> /dev/null
-cp -r /var/lib/scrz-prem/ /root/backup/scrz-prem &> /dev/null
+cp -r /var/lib/geovpnstore/ /root/backup/geovpnstore &> /dev/null
 cp -r /home/sstp /root/backup/sstp &> /dev/null
 cp -r /etc/v2ray /root/backup/v2ray &> /dev/null
 cp -r /etc/xray /root/backup/xray &> /dev/null
@@ -132,7 +132,7 @@ Save_And_Exit () {
     git commit -m m &> /dev/null
     git branch -M main &> /dev/null
     git remote add origin https://github.com/geovpn/user-backup-db
-    git push -f https://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@github.com/geovpn/user-backup-db.git &> /dev/null
+    git push -f https://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@github.com/geovpn/user-backup-db.git &> /dev/null
 }
 
 if [ ! -d "/root/user-backup/" ]; then
