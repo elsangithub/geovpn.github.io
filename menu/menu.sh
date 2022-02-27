7f37b300ba1d18b2891ff9faac7089a35f977526
Muhammad Amin <96859296+geovpn@users.noreply.github.com>
2022-02-27 16:02:48 +0800

    Update menu.sh

--- menu/menu.sh
+++ menu/menu.sh
@@ -1,135 +1,161 @@
 #!/bin/bash
+MYIP=$(curl -sS ipv4.icanhazip.com)
+#########################
+IZIN=$(curl -sS https://raw.githubusercontent.com/geovpn/perizinan/main/main/allow | awk '{print $4}' | grep $MYIP)
+if [ $MYIP = $IZIN ]; then
+echo -e "\e[32mPermission Accepted...\e[0m"
+else
+echo -e "\e[31mPermission Denied!\e[0m";
+echo -e "\e[31mDaftar IP dalam github lok sayang okay? mun dah daftar tapi masih juak permission denied refresh dolok website ya hehe. Love you #\e[0m"
+exit 0
+fi
+#EXPIRED
+expired=$(curl -sS https://raw.githubusercontent.com/geovpn/perizinan/main/main/allow | grep $MYIP | awk '{print $3}')
+echo $expired > /root/expired.txt
+today=$(date -d +1day +%Y-%m-%d)
+while read expired
+do
+	exp=$(echo $expired | curl -sS https://raw.githubusercontent.com/geovpn/perizinan/main/main/allow | grep $MYIP | awk '{print $3}')
+	if [[ $exp < $today ]]; then
+		Exp2="\033[1;31mExpired\033[0m"
+        else
+        Exp2=$(curl -sS https://raw.githubusercontent.com/geovpn/perizinan/main/main/allow | grep $MYIP | awk '{print $3}')
+	fi
+done < /root/expired.txt
+rm /root/expired.txt
+Name=$(curl -sS https://raw.githubusercontent.com/geovpn/perizinan/main/main/allow | grep $MYIP | awk '{print $2}')
+# Color Validation
+#DF='\e[39m'
+#Bold='\e[1m'
+#Blink='\e[5m'
+#yell='\e[33m'
+#red='\e[31m'
+#green='\e[32m'
+#blue='\e[34m'
+#PURPLE='\e[35m'
+#cyan='\e[36m'
+#Lred='\e[91m'
+#Lgreen='\e[92m'
+#Lyellow='\e[93m'
+#NC='\e[0m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+LIGHT='\033[0;37m'
 GREEN='\033[0;32m'
 PURPLE='\033[0;35m'
 CYAN='\033[0;36m'
 NC='\033[0m'
-yl='\e[32;1m'
-bl='\e[36;1m'
-gl='\e[32;1m'
-rd='\e[31;1m'
-mg='\e[0;95m'
-blu='\e[34m'
-op='\e[35m'
-or='\033[1;33m'
+#yl='\e[32;1m'
+#bl='\e[36;1m'
+#gl='\e[32;1m'
+#rd='\e[31;1m'
+#mg='\e[0;95m'
+#blu='\e[34m'
+#op='\e[35m'
+#or='\033[1;33m'
 bd='\e[1m'
 color1='\e[031;1m'
 color2='\e[34;1m'
 color3='\e[0m'
-clear
-dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
-biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
-#########################
-
-BURIQ () {
-    curl -sS https://raw.githubusercontent.com/geovpn/perizinan/main/main/allow > /root/tmp
-    data=( `cat /root/tmp | grep -E "^### " | awk '{print $2}'` )
-    for user in "${data[@]}"
-    do
-    exp=( `grep -E "^### $user" "/root/tmp" | awk '{print $3}'` )
-    d1=(`date -d "$exp" +%s`)
-    d2=(`date -d "$biji" +%s`)
-    exp2=$(( (d1 - d2) / 86400 ))
-    if [[ "$exp2" -le "0" ]]; then
-    echo $user > /etc/.$user.ini
-    else
-    rm -f /etc/.$user.ini > /dev/null 2>&1
-    fi
-    done
-    rm -f /root/tmp
-}
-
-MYIP=$(curl -sS ipinfo.io/ip)
-Name=$(curl -sS https://raw.githubusercontent.com/geovpn/perizinan/main/main/allow | grep $MYIP | awk '{print $2}')
-echo $Name > /usr/local/etc/.$Name.ini
-CekOne=$(cat /usr/local/etc/.$Name.ini)
-
-Bloman () {
-if [ -f "/etc/.$Name.ini" ]; then
-CekTwo=$(cat /etc/.$Name.ini)
-    if [ "$CekOne" = "$CekTwo" ]; then
-        res="Expired"
-    fi
-else
-res="Permission Accepted..."
+# VPS Information
+#Domain
+domain=$(cat /etc/xray/domain)
+#Status certificate
+#modifyTime=$(stat $HOME/.acme.sh/${domain}_ecc/${domain}.key | sed -n '7,6p' | awk '{print $2" "$3" "$4" "$5}')
+modifyTime1=$(date +%s -d "${modifyTime}")
+currentTime=$(date +%s)
+stampDiff=$(expr ${currentTime} - ${modifyTime1})
+days=$(expr ${stampDiff} / 86400)
+remainingDays=$(expr 90 - ${days})
+tlsStatus=${remainingDays}
+if [[ ${remainingDays} -le 0 ]]; then
+	tlsStatus="expired"
 fi
-}
-
-PERMISSION () {
-    MYIP=$(curl -sS ipinfo.io/ip)
-    IZIN=$(curl -sS https://raw.githubusercontent.com/geovpn/perizinan/main/main/allow | awk '{print $4}' | grep $MYIP)
-    if [ "$MYIP" = "$IZIN" ]; then
-    Bloman
-    else
-    res="Permission Denied!"
-    fi
-    BURIQ
-}
-red='\e[1;31m'
-green='\e[1;32m'
-NC='\e[0m'
-green() { echo -e "\\033[32;1m${*}\\033[0m"; }
-red() { echo -e "\\033[31;1m${*}\\033[0m"; }
-PERMISSION
-
-if [ -f /home/needupdate ]; then
-red "Your script need to update first !"
-exit 0
-elif [ "$res" = "Permission Accepted..." ]; then
-echo -ne
-else
-red "Permission Denied!"
-exit 0
-fi
-clear
-PERMISSION
-
-if [ "$res" = "Expired" ]; then
-Exp="\033[1;31mExpired\033[0m"
-rm -f /home/needupdate > /dev/null 2>&1
-else
-Exp=$(curl -sS https://raw.githubusercontent.com/geovpn/perizinan/main/main/allow | grep $MYIP | awk '{print $3}')
-fi
-chck_b(){
-	PID=`ps -ef |grep -v grep | grep scvps_bot |awk '{print $2}'`
-	if [[ ! -z "${PID}" ]]; then
-			sts="\033[0;32m◉ \033[0m"
-		else
-			sts="\033[1;31m○ \033[0m"
-    fi
-}
-echo -e ""
+# OS Uptime
+uptime="$(uptime -p | cut -d " " -f 2-10)"
+# Download
+#Download/Upload today
+dtoday="$(vnstat -i eth0 | grep "today" | awk '{print $2" "substr ($3, 1, 1)}')"
+utoday="$(vnstat -i eth0 | grep "today" | awk '{print $5" "substr ($6, 1, 1)}')"
+ttoday="$(vnstat -i eth0 | grep "today" | awk '{print $8" "substr ($9, 1, 1)}')"
+#Download/Upload yesterday
+dyest="$(vnstat -i eth0 | grep "yesterday" | awk '{print $2" "substr ($3, 1, 1)}')"
+uyest="$(vnstat -i eth0 | grep "yesterday" | awk '{print $5" "substr ($6, 1, 1)}')"
+tyest="$(vnstat -i eth0 | grep "yesterday" | awk '{print $8" "substr ($9, 1, 1)}')"
+#Download/Upload current month
+dmon="$(vnstat -i eth0 -m | grep "`date +"%b '%y"`" | awk '{print $3" "substr ($4, 1, 1)}')"
+umon="$(vnstat -i eth0 -m | grep "`date +"%b '%y"`" | awk '{print $6" "substr ($7, 1, 1)}')"
+tmon="$(vnstat -i eth0 -m | grep "`date +"%b '%y"`" | awk '{print $9" "substr ($10, 1, 1)}')"
+# Getting CPU Information
+cpu_usage1="$(ps aux | awk 'BEGIN {sum=0} {sum+=$3}; END {print sum}')"
+cpu_usage="$((${cpu_usage1/\.*} / ${corediilik:-1}))"
+cpu_usage+=" %"
 ISP=$(curl -s ipinfo.io/org | cut -d " " -f 2-10 )
-#DOMAIN=$(cat /etc/xray/domain)
 CITY=$(curl -s ipinfo.io/city )
-#RAM
+WKT=$(curl -s ipinfo.io/timezone )
+#Sver=$(cat /home/version)
+#tele=$(cat /home/contact)
+DAY=$(date +%A)
+DATE=$(date +%m/%d/%Y)
+IPVPS=$(curl -s ipinfo.io/ip )
+cname=$( awk -F: '/model name/ {name=$2} END {print name}' /proc/cpuinfo )
+cores=$( awk -F: '/model name/ {core++} END {print core}' /proc/cpuinfo )
+freq=$( awk -F: ' /cpu MHz/ {freq=$2} END {print freq}' /proc/cpuinfo )
 tram=$( free -m | awk 'NR==2 {print $2}' )
+uram=$( free -m | awk 'NR==2 {print $3}' )
+fram=$( free -m | awk 'NR==2 {print $4}' )
+clear 
 echo -e ""
 echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
 echo -e "\E[44;1;39m                     ⇱ INFORMASI VPS ⇲                        \E[0m"
 echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
-echo -e " ❇️$bd ISP Name          ${color1} •${color3}$bd $ISP"
-echo -e " ❇️$bd City              ${color1} •${color3}$bd $CITY"
-echo -e " ❇️$bd Total RAM         ${color1} •${color3}$bd $tram MB"
-echo -e " ❇️$bd IP VPS            ${color1} •${color3}$bd $MYIP"
-echo -e " ❇️$bd Client Name       ${color1} •${color3}$bd $Name"
-echo -e " ❇️$bd Expiry script     ${color1} •${color3}$bd $Exp"
+echo -e " ❇️$bd CPU Model            ${color1}•${color3}$bd $cname"
+echo -e " ❇️$bd CPU Frequency        ${color1}•${color3}$bd $freq MHz"
+echo -e " ❇️$bd Number Of Cores      ${color1}•${color3}$bd  $cores"
+echo -e " ❇️$bd CPU Usage            ${color1}•${color3}$bd  $cpu_usage"
+echo -e " ❇️$bd Operating System     ${color1}•${color3}$bd  "`hostnamectl | grep "Operating System" | cut -d ' ' -f5-`	
+echo -e " ❇️$bd Kernel               ${color1}•${color3}$bd  `uname -r`"
+echo -e " ❇️$bd Total Amount Of RAM  ${color1}•${color3}$bd  $tram MB"
+echo -e " ❇️$bd Used RAM             ${color1}•${color3}$bd $red $uram\e[0m MB"
+echo -e " ❇️$bd Free RAM             ${color1}•${color3}$bd  $fram MB"
+echo -e " ❇️$bd System Uptime        ${color1}•${color3}$bd  $uptime "
+echo -e " ❇️$bd Isp Name             ${color1}•${color3}$bd  $ISP"
+echo -e " ❇️$bd Domain               ${color1}•${color3}$bd  $domain"	
+echo -e " ❇️$bd Ip Vps               ${color1}•${color3}$bd  $IPVPS"	
+echo -e " ❇️$bd City                 ${color1}•${color3}$bd  $CITY"
+echo -e " ❇️$bd Time                 ${color1}•${color3}$bd  $WKT"
+echo -e " ❇️$bd Day                  ${color1}•${color3}$bd  $DAY"
+echo -e " ❇️$bd Date                 ${color1}•${color3}$bd  $DATE"
+echo -e " ❇️$bd Telegram             ${color1}•${color3}$bd  @sampiiiiu"
+#echo -e " ❇️$bd Script Version       ${color1} •${color3}$bd  $Sver"
+echo -e " ❇️$bd Certificate status   ${color1}•${color3}$bd  \e[33mExpired in ${tlsStatus} days\e[0m"
+echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
+echo -e " ❇️$bd Traffic\e[0m       \e[33mToday      Yesterday     Month   "
+echo -e " ❇️$bd Download\e[0m      $dtoday    $dyest       $dmon   \e[0m"
+echo -e " ❇️$bd Upload\e[0m        $utoday    $uyest       $umon   \e[0m"
+echo -e " ❇️$bd Total\e[0m       \033[0;36m  $ttoday    $tyest       $tmon  \e[0m "
+echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
 echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
 echo -e "\E[44;1;39m                     ⇱ MENU  OPTIONS ⇲                        \E[0m"
 echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
 echo -e "
-[${green}01${NC}]${color1} •${color3}$bd SSH & OpenVPN Section (${color2}menu-ssh${color3})
-[${green}02${NC}]${color1} •${color3}$bd SSTP - L2TP - PPTP - Wireguard Section (${color2}menu-vpn${color3})
-[${green}03${NC}]${color1} •${color3}$bd Shadowsocks & ShadowsocksR Section (${color2}menu-shadowsocks${color3})
-[${green}04${NC}]${color1} •${color3}$bd V2ray Vmess & Vless Section (${color2}menu-v2ray${color3})
-[${green}05${NC}]${color1} •${color3}$bd Trojan & TrojanGO Section (${color2}menu-trojan${color3})
-[${green}06${NC}]${color1} •${color3}$bd Backup Section (${color2}menu-backup${color3})
-[${green}07${NC}]${color1} •${color3}$bd Trial Account Section (${color2}menu-trial${color3})
-[${green}08${NC}]${color1} •${color3}$bd System Tools Section (${color2}menu-tools${color3})
+[${GREEN} 1 ${NC}]${color1} •${color3}$bd SSH & OpenVPN Section (${color2}menu-ssh${color3})
+[${GREEN} 2 ${NC}]${color1} •${color3}$bd SSTP - L2TP - PPTP - Wireguard Section (${color2}menu-vpn${color3})
+[${GREEN} 3 ${NC}]${color1} •${color3}$bd Shadowsocks & ShadowsocksR Section (${color2}menu-shadowsocks${color3})
+[${GREEN} 4 ${NC}]${color1} •${color3}$bd V2ray Vmess & Vless Section (${color2}menu-v2ray${color3})
+[${GREEN} 5 ${NC}]${color1} •${color3}$bd Trojan & TrojanGO Section (${color2}menu-trojan${color3})
+[${GREEN} 6 ${NC}]${color1} •${color3}$bd Backup Section (${color2}menu-backup${color3})
+[${GREEN} 7 ${NC}]${color1} •${color3}$bd Trial Account Section (${color2}menu-trial${color3})
+[${GREEN} 8 ${NC}]${color1} •${color3}$bd System Tools Section (${color2}menu-tools${color3})
 
 [${green}00${NC}]${color1} •${color3}$bd Back to exit Menu \033[1;32m<\033[1;33m<\033[1;31m<\033[1;31m"
 echo ""
 echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
 echo -e "\E[44;1;39m                     ⇱ GEOVPN PROJECT ⇲                       \E[0m"
+echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
+echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
+echo -e " \e[33mClient Name    ${color1}•${color3}$bd $Name"
+echo -e " \e[33mScript Expired ${color1}•${color3}$bd $Exp2"
 echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
 echo -e  ""
  read -p "  Select menu :  " menu
