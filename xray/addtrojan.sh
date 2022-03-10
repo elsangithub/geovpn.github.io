500eaaaadc9127993bbc9d1c36613175460023f4
Muhammad Amin <96859296+geovpn@users.noreply.github.com>
2022-03-10 09:31:47 +0800

    Add files via upload

--- xray/addtrojan.sh
+++ xray/addtrojan.sh
@@ -84,13 +84,23 @@ domain=$IP
 fi
 tr="$(cat ~/log-install.txt | grep -w "Trojan" | cut -d: -f2|sed 's/ //g')"
 until [[ $user =~ ^[a-zA-Z0-9_]+$ && ${user_EXISTS} == '0' ]]; do
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "\E[44;1;39m         ⇱ TROJAN ACCOUNT ⇲        \E[0m"
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 		read -rp "Password : " -e user
 		user_EXISTS=$(grep -w $user /etc/xray/trojan.json | wc -l)
 
 		if [[ ${user_EXISTS} == '1' ]]; then
+		clear
+			echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+		echo -e "\E[44;1;39m         ⇱ TROJAN ACCOUNT ⇲        \E[0m"
+		echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 			echo ""
-			echo -e "Username ${RED}${user}${NC} Already On VPS Please Choose Another"
-			exit 1
+			echo "A client with the specified name was already created, please choose another name."
+			echo ""
+			echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+			read -n 1 -s -r -p "Press any key to back on menu"
+			menu-trojan
 		fi
 	done
 read -p "Expired (Days) : " masaaktif
@@ -104,26 +114,24 @@ trojanlink1="trojan://${user}@${MYIP}:${tr}?sni=${BUG}#${user}@Geo_Project"
 trojanlink2="trojan://${user}@${domain}:${tr}?sni=${BUG}#${user}@Geo_Project"
 service cron restart
 clear
-echo -e ""
-echo -e "======-XRAYS/TROJAN-======"
-echo -e "Remarks  : ${user}"
-echo -e "IP/Host  : ${MYIP}"
-echo -e "Address  : ${domain}"
-echo -e "Port     : ${tr}"
-echo -e "Key      : ${user}"
-echo -e "Created  : $hariini"
-echo -e "Expired  : $exp"
-echo -e "=========================="
-echo -e "IP + BUG     : "
-echo -e "${trojanlink1}"
-echo -e ""
-echo -e "DOMAIN + BUG : "
-echo -e "${trojanlink2}"
-echo -e "=========================="
-echo -e "Script By geovpn"
-echo -e ""
-echo -e ""
-echo -e ""
-read -n 1 -s -r -p "Press Any Key To Back On Menu"
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
+echo -e "\E[44;1;39m  ⇱ TROJAN ACCOUNT  ⇲ \E[0m" | tee -a /etc/log-create-user.log
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
+echo -e "Remarks      : ${user}" | tee -a /etc/log-create-user.log
+echo -e "Host/IP      : ${domain}" | tee -a /etc/log-create-user.log
+echo -e "port         : ${tr}" | tee -a /etc/log-create-user.log
+#echo -e "Key         : ${uuid}" | tee -a /etc/log-create-user.log
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
+echo -e "IP + BUG     :" | tee -a /etc/log-create-user.log
+echo -e "${trojanlink1}" | tee -a /etc/log-create-user.log
+echo -e "" | tee -a /etc/log-create-user.log
+echo -e "DOMAIN + BUG :" | tee -a /etc/log-create-user.log
+echo -e "${trojanlink2}" | tee -a /etc/log-create-user.log
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
+echo -e "Created      : $hariini" | tee -a /etc/log-create-user.log
+echo -e "Expired      : $exp" | tee -a /etc/log-create-user.log
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
+echo "" | tee -a /etc/log-create-user.log
+read -n 1 -s -r -p "Press any key to back on menu"
 
 menu-trojan
