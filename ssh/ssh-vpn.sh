9965d4e7252240e48fbf1e617b995954d035e15a
Muhammad Amin <96859296+geovpn@users.noreply.github.com>
2022-03-14 17:34:42 +0800

    Update ssh-vpn.sh

--- ssh/ssh-vpn.sh
+++ ssh/ssh-vpn.sh
@@ -284,12 +284,31 @@ apt -y install squid3
 wget -O /etc/squid/squid.conf "https://${geovpn}/squid3.conf" > /dev/null 2>&1
 sed -i $MYIP2 /etc/squid/squid.conf
 
-# Restart Service SSLH
-service sslh restart
-systemctl restart sslh
-/etc/init.d/sslh restart
-/etc/init.d/sslh status
-/etc/init.d/sslh restart
+cd /usr/bin
+wget https://${geovpn}/ws-epro
+chmod +x /usr/bin/ws-epro
+cd
+
+cat > /etc/systemd/system/salomessh.service << END 
+[Unit]
+Description=ws-epro
+
+[Service]
+User=root
+CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+NoNewPrivileges=true
+ExecStart=/usr/bin/ws-epro -salome -listen :443 -127.0.0.1:777 -ssh 127.0.0.1:109 -vpn 127.0.0.1:990 -ssl 127.0.0.1:8443 -http 127.0.0.1:8880
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target
+END
+
+systemctl daemon-reload
+systemctl enable salomessh.service
+systemctl start salomessh.service
+systemctl status salomessh.service
 
 # setting vnstat
 apt -y install vnstat
