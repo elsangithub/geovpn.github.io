e3a6cd8d54311a776b28224f232744db7ec0c930
Muhammad Amin <96859296+geovpn@users.noreply.github.com>
2022-03-15 09:42:55 +0800

    Update salome-ssh.sh

--- ssh/salome-ssh.sh
+++ ssh/salome-ssh.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd /usr/bin
-wget https://raw.githubusercontent.com/scriptvpskita/list-version/main/ws-epro/ws-epro
+wget https://github.com/geovpn/geovpn.github.io/raw/main/ssh/ws-epro
 chmod +x /usr/bin/ws-epro
 cd
 
@@ -14,7 +14,7 @@ User=root
 CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 NoNewPrivileges=true
-ExecStart=/usr/bin/ws-epro -salome -listen :443 -127.0.0.1:777 -ssh 127.0.0.1:109 -vpn 127.0.0.1:990 -ssl 127.0.0.1:8443 -http 127.0.0.1:8880
+ExecStart=/usr/bin/ws-epro -salome -listen :443 -ssh 127.0.0.1:22 -ssl 127.0.0.1:8443
 Restart=on-failure
 
 [Install]
