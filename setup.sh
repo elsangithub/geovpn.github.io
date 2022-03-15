b48ec8ac3fbfcbc2a1aac4b0fee1900ca489f699
Muhammad Amin <96859296+geovpn@users.noreply.github.com>
2022-03-15 13:44:02 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
-if [ "${EUID}" -ne 0 ]; then
-		echo "You need to run this script as root"
-		exit 1
-fi
-if [ "$(systemd-detect-virt)" == "openvz" ]; then
-		echo "OpenVZ is not supported"
-		exit 1
-fi
-# My Telegram : https://t.me/geovpn
 # ==========================================
 dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
 biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
